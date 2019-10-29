5f381faaebbf87ab6e96c402744b8b5c3e8f592e
Spencer Florence <spencer@florence.io>
2019-10-29 14:17:51 -0500

    finish updating to newest circuitous interface

--- circuits/tests/random-tests.rkt
+++ circuits/tests/random-tests.rkt
@@ -8,8 +8,12 @@
 
 (define counter
   (hash-copy
-   (for/hash ([k (in-list (reduction-relation->rule-names ⟶))])
+   (for/hash ([k (in-list (reduction-relation->rule-names ⟶))]
+              #:unless
+              (regexp-match? #rx"(set|var|shared|if-)" (symbol->string k)))
      (values (symbol->string k) 0))))
+(define ATTEMPTS 100)
+
 (redex-check
  esterel-check
  p-pure
@@ -31,29 +35,29 @@
       (constructive->classical
        (compile-esterel/guard x))))
    #;(displayln 'done))
- #:attempts 150)
+ #:attempts ATTEMPTS)
 
 (redex-check
  esterel-check
- p-pure-ext
+ (ρ θ-pure A p-pure-ext)
  (begin
    #;(displayln (term p-pure-ext))
-   (for ([x* (in-list (apply-reduction-relation/tag-with-names ⟶ (term p-pure-ext)))])
+   (for ([x* (in-list (apply-reduction-relation/tag-with-names ⟶ (term (ρ θ-pure A p-pure-ext))))])
      (match-define (list name x) x*)
      (hash-set! counter name
                 (add1 (hash-ref counter name)))
      (assert-same
       #:constraints '(implies (+ SEL) (- GO))
       (constructive->classical
-       (compile-esterel (term p-pure-ext)))
+       (compile-esterel (term (ρ θ-pure A p-pure-ext))))
       (constructive->classical
        (compile-esterel x)))
      (assert-same
       (constructive->classical
-       (compile-esterel/guard (term p-pure-ext)))
+       (compile-esterel/guard (term (ρ θ-pure A p-pure-ext))))
       (constructive->classical
        (compile-esterel/guard x)))))
- #:attempts 150)
+ #:attempts ATTEMPTS)
 
 (pretty-print
  counter)
