a1a0c2190e63b5bbe39555f0ecd853f2e9f04bc8
Robby Findler <robby@racket-lang.org>
2020-03-05 13:32:18 -0600

    non constructive cycle example

--- wg28-2020/talk.rkt
+++ wg28-2020/talk.rkt
@@ -145,6 +145,82 @@
   (slide (aterm->pict (add-right-finger (add-left-finger 0⊥1 '(2 2 1 3)) '(2 2 2 2))))
   (slide (aterm->pict (add-right-finger a011 '(2 2 2 2)))))
 
+(define (nonconstructive-cycle-example)
+  (define an-aterm
+    (aterm (signal S1
+             (signal S2
+               (par
+                (present S1
+                         nothing
+                         (emit S2))
+                (present S2
+                         nothing
+                         (emit S1)))))))
+
+  (define (add-S1-arrow label an-aterm)
+    (add-arc an-aterm
+             '(2 2 2 3 1)
+             rc-find
+             '(2 2 1 1)
+             rc-find
+             (cons 130 -100)
+             label
+             #:start-pull 1
+             #:end-pull 1.2
+             #:start-angle 0
+             #:end-angle pi
+             ))
+
+  (define (add-S2-nested-arrow label an-aterm)
+    (add-arc an-aterm
+             '(2 2 1 3 1)
+             rc-find
+             '(2 2 2 1)
+             rc-find
+             (cons 80 10)
+             label
+             #:start-pull 1
+             #:end-pull 1
+             #:start-angle 0
+             #:end-angle pi
+             ))
+
+  (define (add-S2-arrow label an-aterm)
+    (add-arc an-aterm
+             '(2 2 3 1)
+             ct-find
+             '(2 2 2 1)
+             lb-find
+             (cons -40 -25)
+             label
+             ))
+
+  (define (with-arrows S1-label S2-nested-label S2-label)
+    (add-S2-nested-arrow
+     S2-nested-label
+     (add-S1-arrow
+      S1-label
+      an-aterm)))
+  
+  (define ⊥⊥⊥ (with-arrows '⊥ '⊥ '⊥))
+  (define ⊥⊥1 (with-arrows '⊥ '⊥ 1))
+  (define 0⊥1 (with-arrows 0 '⊥ 1))
+  (define a011 (with-arrows 0 1 1))
+  
+  (slide (aterm->pict an-aterm))
+  (slide (aterm->pict ⊥⊥⊥))
+  
+  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '())))
+  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2))))
+  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2 2))))
+  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2 2 1) '(2 2 2))))
+;  (slide (aterm->pict (add-left-finger ⊥⊥1 '(2 2 1) '(2 2 2))))
+;  (slide (aterm->pict (add-right-finger (add-left-finger 0⊥1 '(2 2 1)) '(2 2 2 2))))
+;  (slide (aterm->pict (add-right-finger (add-left-finger 0⊥1 '(2 2 1 3)) '(2 2 2 2))))
+;  (slide (aterm->pict (add-right-finger a011 '(2 2 2 2))))
+  )
+
 ;(example1)
 ;(example2)
-(constructive-cycle-example)
+;(constructive-cycle-example)
+(nonconstructive-cycle-example)
