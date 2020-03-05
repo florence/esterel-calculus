e0df455779814373e0273577148cfd22f133f3c7
Robby Findler <robby@racket-lang.org>
2020-03-05 16:09:09 -0600

    implement another slide

--- wg28-2020/talk.rkt
+++ wg28-2020/talk.rkt
@@ -83,44 +83,6 @@
 
 (define (constructive-cycle-example)
 
-  (define (add-S1-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 2 3 1)
-             rc-find
-             '(2 2 1 1)
-             rc-find
-             (cons 130 -100)
-             label
-             #:start-pull 1
-             #:end-pull 1.2
-             #:start-angle 0
-             #:end-angle pi
-             ))
-
-  (define (add-S2-nested-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 1 3 1)
-             rc-find
-             '(2 2 2 1)
-             rc-find
-             (cons 80 10)
-             label
-             #:start-pull 1
-             #:end-pull 1
-             #:start-angle 0
-             #:end-angle pi
-             ))
-
-  (define (add-S2-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 3 1)
-             ct-find
-             '(2 2 2 1)
-             lb-find
-             (cons -40 -25)
-             label
-             ))
-
   (define (with-arrows S1-label S2-nested-label S2-label)
     (add-S2-arrow
      S2-label
@@ -158,67 +120,132 @@
                        (emit S1)))))))
 
 (define (nonconstructive-cycle-example)
-  
-  (define (add-S1-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 2 3 1)
-             rc-find
-             '(2 2 1 1)
-             rc-find
-             (cons 130 -100)
-             label
-             #:start-pull 1
-             #:end-pull 1.2
-             #:start-angle 0
-             #:end-angle pi
-             ))
 
-  (define (add-S2-nested-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 1 3 1)
-             rc-find
-             '(2 2 2 1)
-             rc-find
-             (cons 80 10)
-             label
-             #:start-pull 1
-             #:end-pull 1
-             #:start-angle 0
-             #:end-angle pi
-             ))
-
-  (define (add-S2-arrow label an-aterm)
-    (add-arc an-aterm
-             '(2 2 3 1)
-             ct-find
-             '(2 2 2 1)
-             lb-find
-             (cons -40 -25)
-             label
-             ))
-
-  (define (with-arrows S1-label S2-nested-label S2-label)
+  (define ⊥⊥
     (add-S2-nested-arrow
-     S2-nested-label
+     '⊥
      (add-S1-arrow
-      S1-label
+      '⊥
       nonconstructive-cycle-aterm)))
   
-  (define ⊥⊥⊥ (with-arrows '⊥ '⊥ '⊥))
-  (define ⊥⊥1 (with-arrows '⊥ '⊥ 1))
-  (define 0⊥1 (with-arrows 0 '⊥ 1))
-  (define a011 (with-arrows 0 1 1))
-  
   (slide (aterm->pict nonconstructive-cycle-aterm))
-  (slide (aterm->pict ⊥⊥⊥))
+  (slide (aterm->pict ⊥⊥))
   
-  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '())))
-  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2))))
-  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2 2))))
-  (slide (aterm->pict (add-left-finger ⊥⊥⊥ '(2 2 1) '(2 2 2))))
+  (slide (aterm->pict (add-left-finger ⊥⊥ '())))
+  (slide (aterm->pict (add-left-finger ⊥⊥ '(2))))
+  (slide (aterm->pict (add-left-finger ⊥⊥ '(2 2))))
+  (slide (aterm->pict (add-left-finger ⊥⊥ '(2 2 1) '(2 2 2))))
   )
+
+(define (nonconstructive-cycle-with-cycle-shown)
+  (slide
+   (aterm->pict
+    (add-present-S1->then-arrow
+     (add-present-S1->else-arrow
+      (add-present-S2->then-arrow
+       (add-present-S2->else-arrow
+        (add-S2-nested-arrow
+         #f
+         (add-S1-arrow
+          #f
+          nonconstructive-cycle-aterm)))))))))
+  
+(define (add-S1-arrow label an-aterm)
+  (add-arc an-aterm
+           '(2 2 2 3 1)
+           rc-find
+           '(2 2 1 1)
+           rc-find
+           (cons 130 -100)
+           label
+           #:start-pull 1
+           #:end-pull 1.2
+           #:start-angle 0
+           #:end-angle pi
+           ))
+
+(define (add-S2-nested-arrow label an-aterm)
+  (add-arc an-aterm
+           '(2 2 1 3 1)
+           rc-find
+           '(2 2 2 1)
+           rc-find
+           (cons 80 10)
+           label
+           #:start-pull 1
+           #:end-pull 1
+           #:start-angle 0
+           #:end-angle pi
+           ))
+
+(define (add-S2-arrow label an-aterm)
+  (add-arc an-aterm
+           '(2 2 3 1)
+           ct-find
+           '(2 2 2 1)
+           lb-find
+           (cons -40 -25)
+           label
+           ))
+
+(define (add-present-S1->else-arrow an-aterm)
+  (add-arc an-aterm
+           (find-path an-aterm 'S1 1)
+           lb-find
+           (find-path an-aterm 'nothing 0)
+           lc-find
+           (cons 130 -100)
+           #f
+           #:start-pull 1
+           #:end-pull 1.2
+           #:start-angle (* 1.1 pi)
+           #:end-angle 0
+           ))
+
+(define (add-present-S1->then-arrow an-aterm)
+  (add-arc an-aterm
+           (find-path an-aterm 'S1 1)
+           lb-find
+           (find-path an-aterm '(emit S2) 0)
+           lc-find
+           (cons 130 -100)
+           #f
+           #:start-pull 1
+           #:end-pull 1.2
+           #:start-angle (* 1.1 pi)
+           #:end-angle 0
+           ))
+
+(define (add-present-S2->else-arrow an-aterm)
+  (add-arc an-aterm
+           (find-path an-aterm 'S2 2)
+           lb-find
+           (find-path an-aterm 'nothing 1)
+           lc-find
+           (cons 130 -100)
+           #f
+           #:start-pull 1
+           #:end-pull 1.2
+           #:start-angle (* 1.1 pi)
+           #:end-angle 0
+           ))
+
+(define (add-present-S2->then-arrow an-aterm)
+  (add-arc an-aterm
+           (find-path an-aterm 'S2 2)
+           lb-find
+           (find-path an-aterm '(emit S1) 0)
+           lc-find
+           (cons 130 -100)
+           #f
+           #:start-pull 1
+           #:end-pull 1.2
+           #:start-angle (* 1.1 pi)
+           #:end-angle 0
+           ))
 
 ;(example1)
 ;(example2)
 ;(constructive-cycle-example)
 ;(nonconstructive-cycle-example)
+(nonconstructive-cycle-with-cycle-shown)
