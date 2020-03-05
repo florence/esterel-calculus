8898bb73a9fe43ec9e1b7880e963c5648051542d
Robby Findler <robby@racket-lang.org>
2020-03-05 06:24:32 -0600

    start on cyclic example

--- wg28-2020/talk.rkt
+++ wg28-2020/talk.rkt
@@ -69,5 +69,64 @@
   (slide (aterm->pict (add-left-finger with-S-1 '(2 2 2)))))
 
 
-(example1)
-(example2)
+(define (constructive-cycle-example)
+  (define an-aterm
+    (aterm (signal S1
+             (signal S2
+               (par (present S1
+                             nothing
+                             (emit S2))
+                    (present S2
+                             nothing
+                             (emit S1))
+                    (emit S2))))))
+
+  (define (add-S1-arrow label an-aterm)
+    (add-arc an-aterm
+             '(1 3 2 2 2)
+             rc-find
+             '(1 1 2 2)
+             rc-find
+             (cons 130 -100)
+             label
+             #:start-pull 1
+             #:end-pull 1.2
+             #:start-angle 0
+             #:end-angle pi
+             ))
+
+  (define (add-S2-nested-arrow label an-aterm)
+    (add-arc an-aterm
+             '(1 3 1 2 2)
+             rc-find
+             '(1 2 2 2)
+             rc-find
+             (cons 80 0)
+             label
+             #:start-pull 1
+             #:end-pull 1
+             #:start-angle 0
+             #:end-angle pi
+             ))
+
+  (define (add-S2-arrow label an-aterm)
+    (add-arc an-aterm
+             '(1 3 2 2)
+             ct-find
+             '(1 2 2 2)
+             lb-find
+             (cons -40 -20)
+             label
+             ))
+  
+  (slide (aterm->pict (add-S2-arrow
+                       '⊥
+                       (add-S2-nested-arrow
+                        '⊥
+                        (add-S1-arrow
+                         '⊥
+                         an-aterm))))))
+
+;(example1)
+;(example2)
+(constructive-cycle-example)
